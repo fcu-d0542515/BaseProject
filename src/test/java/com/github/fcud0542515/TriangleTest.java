ef287284cc8ad61660518b7eed0e7124ac2d0834
PC234 <win860927@gmail.com.tw>
2017-12-06 12:03:53 +0800

    123

---
+++ src/test/java/com/github/fcud0542515/TriangleTest.java
@@ -0,0 +1,12 @@
+package com.github.fcud0542515;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+    }
+
+}
